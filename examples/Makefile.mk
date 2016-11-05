4633fccf510a2a5277a5df6f095828dd1905a7be
Gerson Fernando Budke <nandojve@gmail.com>
2016-11-05 09:34:38 -0200

    libraries: openthread: scripts: Rem unnecessary script files
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- examples/Makefile.mk
+++ examples/Makefile.mk
@@ -2,8 +2,6 @@
 # Library OpenThread (OT) - Examples Rules
 ################################################################################
 
-LIBRARY_STACK_OPENTHREAD_EXAMPLES_APPS_DIR		= $(LIBRARY_STACK_OPENTHREAD_EXAMPLES_DIR)/apps
 LIBRARY_STACK_OPENTHREAD_EXAMPLES_PLATFORM_DIR	= $(LIBRARY_STACK_OPENTHREAD_EXAMPLES_DIR)/platforms
 
-include $(LIBRARY_STACK_OPENTHREAD_EXAMPLES_APPS_DIR)/Makefile.mk
 include $(LIBRARY_STACK_OPENTHREAD_EXAMPLES_PLATFORM_DIR)/Makefile.mk
--- examples/apps/cli/Makefile.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Library OpenThread (OT) - Examples/Platform/cli Rules
-################################################################################
-
-LIB_CSRC							+=											\
-	$(LIBRARY_STACK_OPENTHREAD_EXAMPLES_APPS_x_DIR)/main.c
-
-INC_PATH								+=										\
-	$(LIBRARY_STACK_OPENTHREAD_EXAMPLES_APPS_x_DIR)
